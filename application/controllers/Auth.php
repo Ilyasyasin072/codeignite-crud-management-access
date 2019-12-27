5f36dbefb65535deacf3286f5610aac5fc256abb
ilyasyasin072 <ilyas@mewahniagajaya.com>
2019-12-27 14:48:30 +0700

    user-activation

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -107,19 +107,123 @@ class Auth extends CI_Controller
 			$this->load->view('auth/registration');
 			$this->load->view('templates/auth_footer');
 		} else {
+			$email = $this->input->post('email', true);
 			$data = [
 				'name' => htmlspecialchars($this->input->post('name', true)),
-				'email' => htmlspecialchars($this->input->post('email', true)),
+				'email' => htmlspecialchars($email),
 				'image' => 'default.jpg',
 				'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
 				'role_id' => 2,
-				'is_active' => 1
+				'is_active' => 0
+			 ];
+
+			 //siapkan token
+
+			$token = base64_encode(random_bytes(32));
+
+			$user_token = [
+				'email' => $email,
+				'token' => $token,
+				'created_at' => time()
 			];
 			$this->db->insert('tb_user', $data);
+			$this->db->insert('tb_user_token', $user_token);
+
+			$this->_sendEmail($token, 'verify');
 			//session alert after registration
 			$this->session->set_flashdata('massage', '
 			<div class="alert alert-success" role="alert">
-			Congratulation your account has been created </div>');
+			Please Active your Account !! </div>');
+			redirect('auth');
+		}
+	}
+
+	public function _sendEmail($token, $type)
+	{
+
+		//send to email untuk aktivasi
+		$config = [
+			'protocol' 	=> 'smtp',
+			'smtp_host' => 'ssl://smtp.googlemail.com',
+			'smtp_user' => 'email pengirim',
+			'smtp_pass' => 'password email',
+			'smtp_port' => 465,
+			'meiltype' 	=> 'html',
+			'charset' 	=> 'utf-8',
+			'newline' 	=> "\r\n"
+		];
+
+		$this->load->library('email', $config);
+		$this->email->initialize($config);
+
+		$this->email->from('email pengirim', 'Management Access');
+		$this->email->to($this->input->post('email'));
+
+		if($type == 'verify') {
+
+		$this->email->subject('Account Verification');
+		$this->email->message('click this link to verify account : <a href="'. base_url() . 'auth/verify?email=' . $this->input->post('email'). '&token='. $token .'">Active<a>');
+
+		}
+
+
+
+		if($this->email->send()){
+			return true;
+		} else {
+			echo $this->email->print_debugger();
+			die;
+		}
+	}
+
+	public function verify()
+	{
+		$email = $this->input->get('email');
+		$token = $this->input->get('token');
+
+		$user = $this->db->get_where('tb_user', ['email' => $email])->row_array();
+		if($user) {
+			$user_token = $this->db->get_where('tb_user_token', ['token' => $token])->row_array();
+			if($user_token) {
+				if(time() - $user_token['created_at'] < (60*60*24)) {
+					$this->db->set('is_active', 1);
+					$this->db->where('email', $email);
+					$this->db->update('tb_user');
+
+					$this->db->delete('tb_user_token', ['email' => $email]);
+
+					$this->session->set_flashdata('massage', '
+					<div class="alert alert-success" role="alert">
+					'.$email.' has been activated pleaste login </div>');
+					
+					redirect('auth');
+				} else {
+
+					$this->db->delete('tb_user', ['email' => $email]);
+					$this->db->delete('tb_user_token', ['email' => $email]);
+
+					$this->session->set_flashdata('massage', '
+					<div class="alert alert-danger" role="alert">
+					Account Activation failed?: Token Expired </div>');
+					
+					redirect('auth');
+				}
+
+			} else {
+
+			$this->session->set_flashdata('massage', '
+			<div class="alert alert-danger" role="alert">
+			Account Activation failed?: Wrong Email </div>');
+			
+			redirect('auth');
+			}
+		
+		} else {
+
+			$this->session->set_flashdata('massage', '
+			<div class="alert alert-danger" role="alert">
+			Account Activation failed?: </div>');
+			
 			redirect('auth');
 		}
 	}
@@ -129,7 +233,7 @@ class Auth extends CI_Controller
 		$this->session->unset_userdata('email');
 		$this->session->unset_userdata('role_id');
 		$this->session->set_flashdata('massage', '
-			<div class="alert alert-success" role="alert">
+			<div class="alert alert-danger" role="alert">
 			you have been logout </div>');
 		redirect('auth');
 	}
