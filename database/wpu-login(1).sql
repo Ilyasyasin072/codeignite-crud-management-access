e27ec80a4a8ba2661eb8a235aab1f5269f3323b2
ilyasyasin072 <ilyas@mewahniagajaya.com>
2019-12-28 14:01:50 +0700

    delete user with sweetalert2

---
+++ database/wpu-login(1).sql
@@ -0,0 +1,238 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 28, 2019 at 08:01 AM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wpu-login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(225) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  `is_active` int(1) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`id`, `name`, `email`, `image`, `password`, `role_id`, `is_active`, `created_at`) VALUES
+(9, 'Administrator', 'admin@example.com', 'index1.jpg', '$2y$10$5AxD0fe/H6o9e/lGs0cBiuYSnsQq3oSYDxkrtpTv8zgrLqKEkgCB6', 1, 1, '2019-12-25 04:56:37'),
+(41, 'Ilyas Yasin Permana', 'ilyasyasin072@gmail.com', 'default.jpg', '$2y$10$bcfpKR3ZZjERCnsgvY/qBeOi1U8yInSeLs7424h3t3g1a73vAwnwW', 2, 1, '2019-12-28 03:57:43');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user_access_menu`
+--
+
+CREATE TABLE `tb_user_access_menu` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tb_user_access_menu`
+--
+
+INSERT INTO `tb_user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 2, 2),
+(4, 1, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user_menu`
+--
+
+CREATE TABLE `tb_user_menu` (
+  `id` int(11) NOT NULL,
+  `nama_menu` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tb_user_menu`
+--
+
+INSERT INTO `tb_user_menu` (`id`, `nama_menu`) VALUES
+(1, 'Admin'),
+(2, 'User'),
+(3, 'Menu');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user_role`
+--
+
+CREATE TABLE `tb_user_role` (
+  `id` int(11) NOT NULL,
+  `role` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_user_role`
+--
+
+INSERT INTO `tb_user_role` (`id`, `role`) VALUES
+(1, 'Administrator'),
+(2, 'Member\r\n');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user_sub_menu`
+--
+
+CREATE TABLE `tb_user_sub_menu` (
+  `id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `icon` varchar(255) NOT NULL,
+  `is_active` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tb_user_sub_menu`
+--
+
+INSERT INTO `tb_user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active`) VALUES
+(1, 1, 'Dashboard', 'admin', 'fas fa-fw fa-tachometer-alt', 1),
+(4, 2, 'User', 'User', 'fas fa-fw fa-user-alt', 1),
+(5, 2, 'Edit Profile', 'user/edit', 'fas fa-fw fa-user-edit', 1),
+(6, 3, 'Menu Management', 'menu', 'fas fa-fw fa-folder', 1),
+(7, 3, 'Submenu Management', 'menu/submenu', 'fas fa-fw fa-folder-open', 1),
+(12, 1, 'Role', 'admin/role', 'fas fa-fw fa-user-tie', 1),
+(15, 1, 'Create User Admin', 'admin/registrationAdmin', 'fas fa-fw fa-user', 1),
+(16, 2, 'Change Password', 'user/changepassword', 'fas fa-fw fa-key', 1),
+(17, 1, 'Show User', 'admin/showuser', 'fas fa-fw fa-tasks', 1),
+(18, 1, 'Role 2', 'user/role', 'fas fa-fw fa-user', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user_token`
+--
+
+CREATE TABLE `tb_user_token` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_user_access_menu`
+--
+ALTER TABLE `tb_user_access_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_user_menu`
+--
+ALTER TABLE `tb_user_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_user_role`
+--
+ALTER TABLE `tb_user_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_user_sub_menu`
+--
+ALTER TABLE `tb_user_sub_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_user_token`
+--
+ALTER TABLE `tb_user_token`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- AUTO_INCREMENT for table `tb_user_access_menu`
+--
+ALTER TABLE `tb_user_access_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tb_user_menu`
+--
+ALTER TABLE `tb_user_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tb_user_role`
+--
+ALTER TABLE `tb_user_role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tb_user_sub_menu`
+--
+ALTER TABLE `tb_user_sub_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `tb_user_token`
+--
+ALTER TABLE `tb_user_token`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
